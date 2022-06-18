a3146d2d3262b46d57bc1c34f12fa9449d15947a
Sergey <serj_bibox@mail.ru>
2022-06-18 16:24:34 +0600

    изменение типа данных

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	n := 0
+	var s string
 	fmt.Print("Введите целое число: ")
-	_, err := fmt.Scan(&n)
+	_, err := fmt.Scan(&s)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %d\n", n)
+	fmt.Printf("Вы ввели число: %s\n", s)
 }
